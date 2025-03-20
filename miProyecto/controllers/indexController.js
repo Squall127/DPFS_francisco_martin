7195d0e27fff4dfd33ad60bbabf46e0cdcebfb02
Squall127 <franproducciones636@gmail.com>
2025-03-20 16:52:16 -0300

    product page structured and some css

--- miProyecto/controllers/indexController.js
+++ miProyecto/controllers/indexController.js
@@ -2,4 +2,10 @@ module.exports = {
     product : (req,res) => {
         return res.render('product')
     },
+    login : (req,res) => {
+        return res.render('login')
+    },
+    register : (req,res) => {
+        return res.render('register')
+    }
 }
